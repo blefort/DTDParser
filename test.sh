084c555110dcda17fb3260d6c23c6f7608fff822
blefort <scribw@gmail.com>
2019-03-04 19:40:07 -0500

    add missing file

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+echo "" > coverage.txt
+for d in $(go list ./... | grep -v vendor); do
+    go test -coverprofile=profile.out -coverpkg=github.com/blefort/DTDParser/DTD,github.com/blefort/DTDParser/parser,github.com/blefort/DTDParser/scanner $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
